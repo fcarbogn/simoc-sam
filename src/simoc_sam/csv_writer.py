73a98b347177b0ad9c4efe67aa1b5f1ca680b675
Franco Carbognani <fcarbogn@gmail.com>
2024-08-25 21:51:29 +0200

    Update csv_writer.py

--- src/simoc_sam/csv_writer.py
+++ src/simoc_sam/csv_writer.py
@@ -1,3 +1,40 @@
+import os
+import csv
+import json
+
+import paho.mqtt.client as mqtt
+
+from simoc_sam.sensors import utils
+
+
+HOST = 'mqtt.simoc.space'
+PORT = 8883
+KEEPALIVE = 10  # in seconds
+TOPIC = "lunares/#"
+
+# Callback when the client connects to the broker
+def on_connect(client, userdata, flags, rc, properties=None):
+    print(f'Connected with result code {rc}')
+    # Subscribe to the MQTT topic
+    client.subscribe(TOPIC)
+
+def on_message(client, userdata, msg):
+    payload = msg.payload.decode("utf-8")
+    topic = msg.topic
+    print(f"Received message: {payload}")
+    print(f"from topic: {topic}")
+    # Parse the payload (assuming it's JSON, adjust as needed)
+    data = json.loads(payload)
+
+    # Define file name based on topic
+    csv_file_path = "/home/data/" + topic.replace("/", "_") + ".csv"
+
+    # Append the data to the CSV file
+    with open(csv_file_path, 'a', newline='') as csv_file:
+        csv_writer = csv.writer(csv_file)
+        #field_names = sorted(data.keys())
+        field_names = ['n', 'timestamp', *[k for k in data if k not in {'n', 'timestamp'}]]
+        # Check if the CSV file is empty
         is_empty = os.stat(csv_file_path).st_size == 0
 
         # Write headers if the file is empty
